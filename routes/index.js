e663308eaf32b07484027cd559e1d88a0233804d
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-20 15:30:01 -0700

    push

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/api', require('./notesRoutes.js'))
+router.use('/api', require('./itemRoutes.js'))
 router.use('/', require('./viewRoutes.js'))
 
 module.exports = router
